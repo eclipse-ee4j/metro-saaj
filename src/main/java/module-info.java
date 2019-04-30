d2c74049f00a477bec1c552fb8825a5ab76b02c2
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-04-30 16:16:32 +0200

    jdk11 and osgi related updates
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -12,13 +12,17 @@
  * Reference Implementation of JSR-67: SOAP with Attachments API for Java.
  */
 module com.sun.xml.messaging.saaj {
-    requires org.jvnet.mimepull;
-    requires java.xml.bind;
-    requires org.jvnet.staxex;
+
+    requires jakarta.activation;
     requires java.xml.soap;
     requires java.logging;
     requires java.desktop;
     requires java.xml;
+    requires org.jvnet.staxex;
+
+    requires static java.xml.bind;
+    requires static com.sun.xml.fastinfoset;
+    requires static org.jvnet.mimepull;
 
     exports com.sun.xml.messaging.saaj;
     exports com.sun.xml.messaging.saaj.packaging.mime;
@@ -32,11 +36,13 @@ module com.sun.xml.messaging.saaj {
     exports com.sun.xml.messaging.saaj.util.transform;
 
     provides javax.xml.soap.MessageFactory
-            with com.sun.xml.messaging.saaj.soap.ver1_1.SOAPMessageFactory1_1Impl;
+            with com.sun.xml.messaging.saaj.soap.ver1_1.SOAPMessageFactory1_1Impl,
+                 com.sun.xml.messaging.saaj.soap.ver1_2.SOAPMessageFactory1_2Impl;
     provides javax.xml.soap.SAAJMetaFactory
             with com.sun.xml.messaging.saaj.soap.SAAJMetaFactoryImpl;
     provides javax.xml.soap.SOAPConnectionFactory
             with com.sun.xml.messaging.saaj.client.p2p.HttpSOAPConnectionFactory;
     provides javax.xml.soap.SOAPFactory
-            with com.sun.xml.messaging.saaj.soap.ver1_1.SOAPFactory1_1Impl;
+            with com.sun.xml.messaging.saaj.soap.ver1_1.SOAPFactory1_1Impl,
+                 com.sun.xml.messaging.saaj.soap.ver1_2.SOAPFactory1_2Impl;
 }
---
+++ src/test/java/classfile/ByteCodeVersionTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package classfile;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author lukas
+ */
+public class ByteCodeVersionTest {
+
+    public ByteCodeVersionTest() {
+    }
+
+    @Test
+    public void testModuleInfoByteCodeVersion() {
+        // module-info for JDK 9
+        verifyClassFileFormat("/module-info.class", 0x35);
+    }
+
+    @Test
+    public void testClassByteCodeVersion() {
+        // class files for JDK 7
+        verifyClassFileFormat("/com/sun/xml/messaging/saaj/soap/SOAPDocument.class", 0x33);
+    }
+    
+    private static void verifyClassFileFormat(String resource, int expectedClassVersion) {
+        try (InputStream in = ByteCodeVersionTest.class.getModule().getResourceAsStream(resource);
+                DataInputStream data = new DataInputStream(in)) {
+            if (0xCAFEBABE != data.readInt()) {
+                Assert.fail("invalid header");
+            }
+            // minor
+            int i = data.readUnsignedShort();
+            // major
+            i = data.readUnsignedShort();
+            Assert.assertEquals("Class Files compiled for wrong java runtime version", expectedClassVersion, i);
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+}
