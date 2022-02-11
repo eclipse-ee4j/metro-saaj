dbdcfa2730cd87fbb58291867782cc96f3c0033e
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-02-11 19:59:23 +0100

    fix test failing on some SE 17 builds
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- saaj-ri/src/test/java/mime/StartParameterTest.java
+++ saaj-ri/src/test/java/mime/StartParameterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -10,14 +10,29 @@
 
 package mime;
 
-import java.io.*;
-import java.net.URL;
-import java.util.*;
-
 import jakarta.activation.DataHandler;
-import jakarta.xml.soap.*;
-
+import jakarta.xml.soap.AttachmentPart;
+import jakarta.xml.soap.MessageFactory;
+import jakarta.xml.soap.MimeHeader;
+import jakarta.xml.soap.MimeHeaders;
+import jakarta.xml.soap.SOAPBody;
+import jakarta.xml.soap.SOAPBodyElement;
+import jakarta.xml.soap.SOAPEnvelope;
+import jakarta.xml.soap.SOAPHeader;
+import jakarta.xml.soap.SOAPMessage;
+import jakarta.xml.soap.SOAPPart;
 import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
 
 public class StartParameterTest extends TestCase {
 
@@ -122,10 +137,14 @@ public class StartParameterTest extends TestCase {
 
         msg.saveChanges();
 
-	FileOutputStream sentFile =
-            new FileOutputStream("target/test-out/message.txt");
-	msg.writeTo(sentFile);
-	sentFile.close();
+        File f = new File("target/test-out/message.txt");
+        f.getParentFile().mkdirs();
+        if (f.exists()) f.delete();
+        f.createNewFile();
+        FileOutputStream sentFile =
+                new FileOutputStream(f);
+        msg.writeTo(sentFile);
+        sentFile.close();
         changeAndSaveMimeHeaders(msg, "target/test-out/headers.txt");
 
         SOAPMessage newMsg =
